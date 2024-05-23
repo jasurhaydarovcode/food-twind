70b2a98bce51ec74ed577bd55860d6db1e4723af
ha7darov <vi.yeoun.vi@gmail.com>
2024-05-23 15:26:39 +0500

    END

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#ff6363',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontFamily: {
+        Nunito: ['Nunito']
+      }
+    },
+  },
+  plugins: [],
+}
+
